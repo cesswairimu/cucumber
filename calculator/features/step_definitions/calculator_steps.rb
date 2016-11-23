a5c1c569e814936228b921afa5ea09ea39ec19c5
Cess <cessmbuguar@gmail.com>
2016-11-23 09:29:04 +0300

    remove brackets from when step

--- calculator/features/step_definitions/calculator_steps.rb
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -1,11 +1,11 @@
-Given(/^the input "([^"]*)"$/) do |input|
-  @input = input
+Given /^the input "([^"]*)"$/ do |input|
+@input = input
 end
 
-When(/^the calculator is run$/) do
 
+When /^the calculator is run$/ do
   @output = `ruby calc.rb #{@input}`
-  raise('Command failed!!') unless $?.success?
+  raise('Command failed!') unless $?.success?
 end
 
 Then /^the output should be "([^"]*)"$/ do |expected_output|
