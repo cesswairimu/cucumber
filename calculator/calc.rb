81faa34dca88c4ec17f7241ae4814ec6bb445019
Cess <cessmbuguar@gmail.com>
2016-11-24 10:13:19 +0300

    a simple addition code

--- calculator/calc.rb
+++ calculator/calc.rb
@@ -1,1 +1,13 @@
-print "4"
+puts "Enter the first number for addition"
+input1 = gets
+puts "Enter the second number for addition"
+input2 = gets
+puts "Your numbers are #{input1} and #{input2}"
+
+puts"_____ Now we will add your numbers______"
+
+sum = input1.to_i + input2.to_i
+
+puts "The sum of the two numbers is #{sum}"
+
+
