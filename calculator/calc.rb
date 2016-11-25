a260d866e6cc0a4391e51207ca630a5efc102c53
Cess <cessmbuguar@gmail.com>
2016-11-25 10:25:02 +0300

    back to simple original code

--- calculator/calc.rb
+++ calculator/calc.rb
@@ -1,13 +1,1 @@
-puts "Enter the first number for addition"
-input1 = gets
-puts "Enter the second number for addition"
-input2 = gets
-puts "Your numbers are #{input1} and #{input2}"
-
-puts"_____ Now we will add your numbers______"
-
-output = input1.to_i + input2.to_i
-
-puts "The sum of the two numbers is #{output}"
-
-
+print "4"
