8f6b1986eebb197f7a1d3697ab9b376309f5287e
Cess <cessmbuguar@gmail.com>
2016-12-05 12:32:31 +0300

    pending step definitions with problem on each scenario

--- step_definitions/features/step_definitions/steps.rb
+++ step_definitions/features/step_definitions/steps.rb
@@ -2,3 +2,11 @@ Given /^I have deposited \$(\d+) in my account$/ do |amount|
   pending("Need to design the account interface")
 end
 
+When /^I request for \$(\d+)$/ do |amount|
+  pending("How do we stimulate cash being requested?")
+end
+
+Then /^\$(\d+) should be dispensed$/ do |amount|
+  pending("How do we validate the amount being dispensed?")
+end
+
